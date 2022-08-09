514e0b77100f22f6c10bffaafb9c67c36dfd672f
Kingsman56 <93114503+Kingsman56@users.noreply.github.com>
2022-08-09 20:47:31 +0530

    Add files via upload

---
+++ app/src/main/res/layout/User.java
@@ -0,0 +1,16 @@
+package com.example.minorproject;
+
+public class User {
+    public String name2 , age2, email3 , policeid2;
+
+    public User(String name2, String age2, String email3){
+
+    }
+
+    public User(String name2 , String age2 , String email3 , String policeid2){
+        this.name2 = name2;
+        this.age2 = age2;
+        this.email3 = email3;
+        this.policeid2 = policeid2;
+    }
+}
